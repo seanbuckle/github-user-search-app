6169539245e99344fbbea5f314de14418f66aa14
seanbuckle <seanlbuckle@gmail.com>
2024-03-25 22:05:24 +0000

    Add header, search, and result components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-function App() {
+import Header from "./components/Header.tsx";
+import Search from "./components/Search.tsx";
+import Result from "./components/Result.tsx";
 
+function App() {
   return (
     <>
-    
+      <Header />
+      <Search />
+      <Result />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
