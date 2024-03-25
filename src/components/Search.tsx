2fa39d26cf64c6d1093cd0d8bfcedad2d2868f8a
seanbuckle <seanlbuckle@gmail.com>
2024-03-25 22:06:32 +0000

    Add Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,13 @@
+function Search() {
+  return (
+    <>
+      <form action="">
+        <input type="text" placeholder="Search GitHub username..." />
+        <label></label>
+        <button>Search</button>
+      </form>
+    </>
+  );
+}
+
+export default Search;
