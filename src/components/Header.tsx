9abffff770cb430030ab5e41622dea38c8006988
seanbuckle <seanlbuckle@gmail.com>
2024-03-25 22:06:25 +0000

    Add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <h1>devfinder</h1>
+        <div>
+          <button>Light</button>
+          <button>Dark</button>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
